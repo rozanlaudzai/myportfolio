7a96cabe16f4dd6bb09b708e9d61deb252a15e00
Rozan Laudzai <rozanlaudzai717@gmail.com>
2026-09-02 15:02:09 +0700

    feat: add public domain in ALLOWED_HOSTS

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.getenv('MY_DOMAIN')]
 
 
 # Application definition
