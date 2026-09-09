acd0b704f6bdbc2f2a85d09481a0fce2299a5e6b
Rozan Laudzai <rozanlaudzai717@gmail.com>
2026-09-09 23:32:35 +0700

    chore(experience-db): use django template lang to render url

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'main'
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('experience/', views.show_experience, name='experience'),
+    path('experience/', views.show_experience, name='show_experience'),
 ]
