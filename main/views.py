9966a12dccd4f3a8e09ba429ba4f74420db33cad
Rozan Laudzai <rozanlaudzai717@gmail.com>
2026-09-09 23:20:09 +0700

    feat(experience-db): create /experience page and its css styling

--- main/views.py
+++ main/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from .models import Experience
 
 def index(request):
-    return render(request, 'main/index.html')
+    context = {
+        'name': 'Rozan',
+        'npm': '2506547544',
+        'study_program': 'S1 Ilmu Komputer',
+        'bio': 'Go & C++ Enjoyer. Python & JavaScript Hater.'
+    }
+    return render(request, 'main/index.html', context)
+
+def show_experience(request):
+    context = {
+        'name': 'Rozan',
+        'experience_list': Experience.objects.all()
+    }
+    return render(request, 'main/experience.html', context)
