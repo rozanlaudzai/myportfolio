5bf44024488404c1bb266b830dcea980761e1034
Rozan Laudzai <rozanlaudzai717@gmail.com>
2026-09-14 23:39:48 +0700

    test(experience): update tests for current model changes

--- main/tests.py
+++ main/tests.py
@@ -1,23 +1,15 @@
+from datetime import date
 from django.test import TestCase
 from django.urls import reverse
-from django.utils import timezone
 
-from .models import Experience
+from .models import Experience, Skill
 
 class MainTest(TestCase):
-    def setUp(self):
-        self.experience = Experience.objects.create(
-            title='Asisten Dosen PBP',
-            description='Membantu mahasiswa memahami pengembangan web.',
-            category='part-time',
-        )
-
     def test_main_url_is_accessible(self):
         response = self.client.get(reverse('main:index'))
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'main/index.html')
-        self.assertNotContains(response, self.experience.title)
         self.assertContains(response, f'href="{reverse("main:index")}"')
 
     def test_nonexistent_page_returns_404(self):
@@ -25,10 +17,32 @@ class MainTest(TestCase):
 
         self.assertEqual(response.status_code, 404)
 
+class ExperienceTest(TestCase):
+    def setUp(self):
+        self.django = Skill.objects.create(
+            name='Django'
+        )
+        self.experience = Experience.objects.create(
+            title='Asisten Dosen PBP',
+            company_name='Universitas Indonesia',
+            company_logo='img/pelihara-logo.webp',
+            description='Membantu mahasiswa memahami pengembangan web.',
+            started_at=date(2026, 8, 1),
+        )
+        self.experience.skills.add(self.django)
+
     def test_experience_model(self):
-        self.assertEqual(str(self.experience), 'Asisten Dosen PBP')
-        self.assertEqual(self.experience.category, 'part-time')
+        self.assertEqual(
+            str(self.experience),
+            'Asisten Dosen PBP at Universitas Indonesia',
+        )
+        self.assertQuerySetEqual(self.experience.skills.all(), [self.django])
         self.assertTrue(self.experience.is_ongoing)
+
+    def test_experience_is_not_shown_on_profile(self):
+        response = self.client.get(reverse('main:index'))
+
+        self.assertNotContains(response, self.experience.title)
 
     def test_experience_page(self):
         response = self.client.get(reverse('main:show_experience'))
@@ -37,8 +51,10 @@ class MainTest(TestCase):
         self.assertTemplateUsed(response, 'main/experience.html')
         self.assertContains(response, self.experience.title)
         self.assertContains(response, self.experience.description)
-        self.assertContains(response, 'Part-Time')
-        self.assertContains(response, 'On Going')
+        self.assertContains(response, self.experience.company_name)
+        self.assertContains(response, '<li>Django</li>', html=True)
+        self.assertContains(response, '<time>Aug 2026</time>', html=True)
+        self.assertContains(response, '<time>Present</time>', html=True)
         self.assertContains(response, f'href="{reverse("main:index")}"')
 
     def test_empty_experience_page(self):
@@ -46,14 +62,16 @@ class MainTest(TestCase):
 
         response = self.client.get(reverse('main:show_experience'))
 
-        self.assertContains(response, 'No experience added.')
+        self.assertContains(response, 'No experience added yet.')
 
     def test_completed_experience(self):
-        self.experience.ended_at = timezone.now()
+        self.experience.ended_at = date(2026, 12, 1)
         self.experience.save()
+        self.experience.refresh_from_db()
 
         response = self.client.get(reverse('main:show_experience'))
 
         self.assertFalse(self.experience.is_ongoing)
-        self.assertContains(response, 'Ended')
-        self.assertNotContains(response, 'On Going')
+        self.assertContains(response, '<time>Aug 2026</time>', html=True)
+        self.assertContains(response, '<time>Dec 2026</time>', html=True)
+        self.assertNotContains(response, '<time>Present</time>', html=True)
