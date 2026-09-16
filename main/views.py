48e636d5987bcd454e608cec5b71822fc36dfbc8
Rozan Laudzai <rozanlaudzai717@gmail.com>
2026-09-17 00:25:58 +0700

    chore(award-search): remove debug print

--- main/views.py
+++ main/views.py
@@ -59,12 +59,8 @@ def get_awards_json(request: HttpRequest):
     title_query = request.GET.get('title', '').strip()
     awards = Award.objects.all()
 
-    print(f'{title_query=}')
-
     if title_query:
         awards = awards.filter(title__icontains=title_query)
-        print(f'{[award for award in awards]}')
-
 
     awards_json = serializers.serialize('json', awards)
     return HttpResponse(awards_json, content_type='application/json')
