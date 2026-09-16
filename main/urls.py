fb642c4559427bcedfa25fe7adf463ec89c8e861
Rozan Laudzai <rozanlaudzai717@gmail.com>
2026-09-16 23:48:01 +0700

    feat(award-form): add delete-award modal & button and search-by-name for awards at /awards/

--- main/urls.py
+++ main/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('experience/', views.show_experience, name='show_experience'),
     path('awards/', views.show_awards, name='show_awards'),
     path('awards/add/', views.create_award, name='create_award'),
+    path('api/awards', views.get_awards_json, name='get_awards_json'),
+    path('awards/<uuid:award_id>/delete/', views.delete_award, name='delete_award'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
 from django.core import serializers
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpRequest, HttpResponse
 
 from .models import Experience, Award
@@ -22,10 +22,22 @@ def show_experience(request):
     }
     return render(request, 'main/experience.html', context)
 
-def show_awards(request):
+def show_awards(request: HttpRequest):
+    json_response = get_awards_json(request)
+
+    awards = serializers.deserialize(
+        'json',
+        json_response.content.decode('utf-8'),
+    )
+
+    awards = [award.object for award in awards]
+
+    title_query = request.GET.get('title', '').strip()
+
     context = {
         'name': 'Rozan',
         'award_list': Award.objects.all(),
+        'title_query': title_query,
     }
     return render(request, 'main/awards.html', context)
 
@@ -42,3 +54,22 @@ def create_award(request: HttpRequest):
         'form': form,
     }
     return render(request, 'main/award-form.html', context)
+
+def get_awards_json(request: HttpRequest):
+    title_query = request.GET.get('title', '').strip()
+    awards = Award.objects.all()
+
+    if title_query:
+        awards = awards.filter(title__icontains=title_query)
+
+    awards_json = serializers.serialize('json', awards)
+    return HttpResponse(awards_json, content_type='application/json')
+
+def delete_award(request: HttpRequest, award_id):
+    award = get_object_or_404(Award, pk=award_id)
+
+    if request.method == 'POST':
+        award.delete()
+        messages.success(request, 'Award successfully deleted!')
+
+    return redirect('main:show_awards')
